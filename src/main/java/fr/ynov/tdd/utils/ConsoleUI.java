51078955db36a520173c5fb77c661b1ea0414175
Fabien Hebuterne <fabien.hebuterne@gmail.com>
2020-03-12 11:07:35 +0100

    feat : console-ui : add unit test

---
+++ src/main/java/fr/ynov/tdd/utils/ConsoleUI.java
@@ -0,0 +1,15 @@
+package fr.ynov.tdd.utils;
+
+import fr.ynov.tdd.domain.HandManState;
+
+public class ConsoleUI {
+
+    public static String showUI(String wordName, HandManState handManState, int currentAttempt, int maxAttempt) {
+        return "";
+    }
+
+    public static void printUIInConsole(String ui) {
+        //System.out.println(ui);
+    }
+
+}
