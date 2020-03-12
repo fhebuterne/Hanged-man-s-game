bc6fb84ed12c3262704fc0e0035214d877e36164
Fabien Hebuterne <fabien.hebuterne@gmail.com>
2020-03-12 11:55:13 +0100

    feat : console-ui : add function to show UI

--- src/main/java/fr/ynov/tdd/utils/ConsoleUI.java
+++ src/main/java/fr/ynov/tdd/utils/ConsoleUI.java
@@ -2,10 +2,23 @@ package fr.ynov.tdd.utils;
 
 import fr.ynov.tdd.domain.HandManState;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ConsoleUI {
 
-    public static String showUI(String wordName, HandManState handManState, int currentAttempt, int maxAttempt) {
-        return "";
+    public static String showUI(String wordName, HandManState handManState, int currentAttempt, int maxAttempt, ArrayList<Character> lettersAlreadyTried) {
+        String ui = handManState.getAsciiArt();
+
+        int lenghtWord =  wordName.replace(" ", "").toCharArray().length;
+        String letters = lettersAlreadyTried.stream().map(String::valueOf).collect(Collectors.joining(","));
+
+        StringBuilder hiddenWord = new StringBuilder();
+        IntStream.range(0, lenghtWord).mapToObj(i -> "_ ").forEachOrdered(hiddenWord::append);
+
+        return MessageFormat.format(ui, currentAttempt, maxAttempt, hiddenWord, letters);
     }
 
     public static void printUIInConsole(String ui) {
--- src/test/java/fr/ynov/tdd/utils/ConsoleUITest.java
+++ src/test/java/fr/ynov/tdd/utils/ConsoleUITest.java
@@ -3,6 +3,9 @@ package fr.ynov.tdd.utils;
 import fr.ynov.tdd.domain.HandManState;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConsoleUITest {
@@ -14,25 +17,26 @@ class ConsoleUITest {
         HandManState stateHandMan = HandManState.HAND_MAN_STATE_0;
         int currentAttempt = 0;
         int maxAttempt = 6;
+        ArrayList<Character> chars = new ArrayList<>(Arrays.asList('A', 'B', 'C'));
 
         String expected =
                         "#################################################################################\n" +
                         "                       #                          #                              \n" +
-                        " ,=============Y====== #                          # Tentatives 0/6               \n" +
+                        " ,=============Y====== #                          # Tentatives 0/6\n" +
                         "    ||  /      |       # Mot à trouver:           #                              \n" +
                         "    || /       |       #                          ###############################\n" +
                         "    ||/                #                                                         \n" +
                         "    ||                 #                                                         \n" +
-                        "    ||                 # _ _ _ _ _ _ _                                           \n" +
+                        "    ||                 # _ _ _ _ _ _ _ _ \n" +
                         "    ||                 #                                                         \n" +
                         "   /||                 ##########################################################\n" +
-                        "  //||                 # Lettres déjà tentées:                                   \n" +
+                        "  //||                 # Lettres déjà tentées: A,B,C\n" +
                         " ===================== #                                                         \n" +
                         "                       #                                                         \n" +
                         "#################################################################################\n";
 
         // when
-        String ui = ConsoleUI.showUI(wordName, stateHandMan, currentAttempt, maxAttempt);
+        String ui = ConsoleUI.showUI(wordName, stateHandMan, currentAttempt, maxAttempt, chars);
 
         // then
         assertEquals(expected, ui);
