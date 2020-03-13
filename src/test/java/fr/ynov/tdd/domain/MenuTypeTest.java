a4b6dce40604facc33ee1728fe43d2b11d5461bd
Fabien Hebuterne <fabien.hebuterne@gmail.com>
2020-03-13 15:44:17 +0100

    chore : refactor one test

--- src/test/java/fr/ynov/tdd/domain/MenuTypeTest.java
+++ src/test/java/fr/ynov/tdd/domain/MenuTypeTest.java
@@ -2,12 +2,19 @@ package fr.ynov.tdd.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MenuTypeTest {
 
     @Test
-    void parseMenu() {
-        assertEquals("### MENU START ###\n1 - Jouer\n2 - Options\n3 - Quitter\nQuel est votre choix ?\n",MenuType.START_APP_MENU.parseMenu());
+    void should_parsing_menu_with_option() {
+        // given
+        String excepted = "### MENU START ###\n1 - Jouer\n2 - Options\n3 - Quitter\nQuel est votre choix ?\n";
+
+        // when
+        String parsing = MenuType.START_APP_MENU.parseMenu();
+
+        // then
+        assertEquals(excepted, parsing);
     }
 }
