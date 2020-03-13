56559a055a8564cd30ca6f346c0fdf7425242587
Vincent Roquelaure <roquelaurevincent@gmail.com>
2020-03-13 10:27:50 +0100

    feat : database : add database statistics methods and refactoring (before was tests methods)

--- src/main/java/fr/ynov/tdd/database/DatabaseProvider.java
+++ src/main/java/fr/ynov/tdd/database/DatabaseProvider.java
@@ -1,5 +1,8 @@
 package fr.ynov.tdd.database;
 
+import fr.ynov.tdd.domain.DatabaseEnum;
+import fr.ynov.tdd.domain.Statistic;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Properties;
@@ -28,6 +31,7 @@ public class DatabaseProvider {
             Statement stmt = connection.createStatement();
             stmt.executeUpdate(DatabaseEnum.CREATE_TABLE_CATEGORY.getValue());
             stmt.executeUpdate(DatabaseEnum.CREATE_TABLE_WORD.getValue());
+            stmt.executeUpdate(DatabaseEnum.CREATE_TABLE_STATISTIC.getValue());
             stmt.executeUpdate(DatabaseEnum.INSERT_DEFAULT_CATEGORY.getValue());
             stmt.close();
         } catch (SQLException sqlE) {
@@ -101,5 +105,45 @@ public class DatabaseProvider {
         }
 
         return words;
+    }
+
+    public void addStatisticsAtEndGame(Statistic statistic) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DatabaseEnum.INSERT_STATISTIC.getValue());
+
+            preparedStatement.setString(1, statistic.getWordToFind());
+            preparedStatement.setBoolean(2, statistic.haveFoundWord());
+            preparedStatement.setInt(3, statistic.getTryCount());
+            preparedStatement.setDate(4, new java.sql.Date(statistic.getDate().getTime()));
+            preparedStatement.execute();
+
+            preparedStatement.close();
+        } catch (SQLException sqlE) {
+            sqlE.printStackTrace();
+        }
+    }
+
+    public ArrayList<Statistic> getAllStatistics() {
+        ArrayList<Statistic> statistics = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DatabaseEnum.GET_STATISTICS.getValue());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                statistics.add(new Statistic(
+                        resultSet.getString("word"),
+                        resultSet.getBoolean("found"),
+                        resultSet.getInt("tryCount"),
+                        resultSet.getDate("date")
+                ));
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException sqlE) {
+            sqlE.printStackTrace();
+        }
+
+        return statistics;
     }
 }
--- src/main/java/fr/ynov/tdd/domain/DatabaseEnum.java
+++ src/main/java/fr/ynov/tdd/domain/DatabaseEnum.java
@@ -1,16 +1,19 @@
-package fr.ynov.tdd.database;
+package fr.ynov.tdd.domain;
 
 public enum DatabaseEnum {
     DATABASE_NAME("hangmandb"),
     DATABASE_USER("hangmandb"),
     DATABASE_PASSWORD("hangmandb"),
     CREATE_TABLE_CATEGORY("CREATE TABLE IF NOT EXISTS Category(id IDENTITY PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50) NOT NULL)"),
-    CREATE_TABLE_WORD("CREATE TABLE IF NOT EXISTS Word(value VARCHAR(50) NOT NULL, categoryId INT NOT NULL, FOREIGN KEY (CategoryId) REFERENCES Category(id))"),
+    CREATE_TABLE_WORD("CREATE TABLE IF NOT EXISTS Word(id IDENTITY PRIMARY KEY AUTO_INCREMENT, value VARCHAR(50) NOT NULL, categoryId INT NOT NULL, FOREIGN KEY (CategoryId) REFERENCES Category(id))"),
+    CREATE_TABLE_STATISTIC("CREATE TABLE IF NOT EXISTS Statistic(id IDENTITY PRIMARY KEY AUTO_INCREMENT, word VARCHAR(50) NOT NULL, found BOOLEAN NOT NULL, tryCount INT, date DATE)"),
     INSERT_DEFAULT_CATEGORY("INSERT INTO Category(name) VALUES('none') ON DUPLICATE KEY UPDATE name='none'"),
     SELECT_CATID_BY_NAME("SELECT id FROM Category WHERE name = ?"),
     INSERT_CARD_WITH_CAT("INSERT INTO Word(value, categoryId) VALUES(?,?)"),
     INSERT_CATEGORIE("INSERT INTO Category(name) VALUES (?)"),
-    SELECT_WORDS_BY_CATID("SELECT value FROM Word WHERE categoryId = ?");
+    SELECT_WORDS_BY_CATID("SELECT value FROM Word WHERE categoryId = ?"),
+    INSERT_STATISTIC("INSERT INTO Statistic(word, found, tryCount, date) VALUES (?,?,?,?)"),
+    GET_STATISTICS("SELECT * FROM Statistic");
 
     public String value;
 
